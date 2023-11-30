73857298f3b536b481e107e72d72f0f11106e5f5
La Tasha Pollard <latashajpollard@gmail.com>
2023-11-29 22:40:42 -0600

    adds UI functionality

---
+++ lib/app.py
@@ -0,0 +1,131 @@
+import sys, os
+import typing
+from PyQt6 import QtCore
+import openai
+from dotenv import load_dotenv
+from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QTextEdit, QSplitter, QHBoxLayout, QVBoxLayout, QStatusBar)
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QFont
+from transcriber import get_video_transcript, summarize_transcript, TranscriptsDisabled
+
+# pyqt6 framework can detect project folder
+def resource_path(relative_path):
+  try:
+    base_path = sys._MEIPASS
+  except Exception:
+    base_path = os.path.abspath(".")
+  return os.path.join(base_path, relative_path)
+
+class myApp(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.window_width, self.window_height = 700, 700
+    self.setMinimumSize(self.window_width, self.window_height)
+    self.setWindowTitle('Youtube Video Summarizer App')
+    # self.setWindowIcon(QIcon('./icon.png'))
+
+    self.layout = {}
+    self.layout['main'] = QVBoxLayout()
+    self.setLayout(self.layout['main'])
+    self.init_UI()
+    self.config_signal()
+
+  def init_UI(self):
+    self.layout['video_id_entry'] = QHBoxLayout()
+    self.layout['main'].addLayout(self.layout['video_id_entry'])
+    self.video_id_input = QLineEdit()
+    self.layout['video_id_entry'].addWidget(QLabel('Video ID:'))
+    self.layout['video_id_entry'].addWidget(self.video_id_input)
+    self.layout['video_id_entry'].addStretch(1)
+    splitter = QSplitter(Qt.Orientation.Vertical)
+    self.layout['main'].addWidget(splitter)
+
+    self.transcription_field = QTextEdit()
+    self.summarized_field = QTextEdit()
+    splitter.addWidget(self.transcription_field)
+    splitter.addWidget(self.summarized_field)
+
+    # prevents splitter from being completely closed
+    splitter.setCollapsible(0, False)
+    splitter.setCollapsible(1, False)
+
+    # add buttons
+    self.layout['button'] = QHBoxLayout()
+    self.layout['main'].addLayout(self.layout['button'])
+    self.btn_transcribe = QPushButton('&Transcribe')
+    self.btn_transcribe.setFixedWidth(100)
+    self.btn_summarize = QPushButton('&Summarize')
+    self.btn_summarize.setFixedWidth(100)
+    self.btn_reset = QPushButton('&Reset')
+    self.btn_reset.setFixedWidth(100)
+    self.layout['button'].addWidget(self.btn_transcribe)
+    self.layout['button'].addWidget(self.btn_summarize)
+    self.layout['button'].addWidget(self.btn_reset)
+    self.layout['button'].addStretch()
+
+    # add status bar
+    self.statusbar = QStatusBar()
+    self.layout['main'].addWidget(self.statusbar)
+  
+  def reset_fields(self):
+    self.video_id_input.clear()
+    self.transcription_field.clear()
+    self.summarized_field.clear()
+    self.statusbar.clearMessage()
+
+  def transcribe_video(self):
+    video_id = self.video_id_input.text()
+    if not video_id:
+      self.statusbar.showMessage('Video ID is missing')
+      return
+    
+    self.statusbar.clearMessage()
+    self.transcription_field.clear()
+    self.summarized_field.clear()
+
+    try: 
+      transcription = get_video_transcript(video_id)
+      self.transcription_field.setPlainText(transcription)
+    except TranscriptsDisabled:
+      self.statusbar.showMessage('Transcription not available')
+    except Exception as e:
+      self.statusbar.showMessage(str(e))
+      
+
+  def summarize_video(self):
+    transcription = self.transcription_field.toPlainText()
+    if not transcription:
+      self.statusbar.showMessage('Transcription is empty')
+    self.summarized_field.clear()
+    self.statusbar.clearMessage()
+
+    try:
+      video_summary = summarize_transcript(transcription)
+      self.summarized_field.setPlainText(video_summary)
+    except Exception as e:
+      # self.statusbar.showMessage(str(e))
+      self.summarized_field.setPlainText(str(e))
+
+
+  def config_signal(self):
+    self.btn_transcribe.clicked.connect(self.transcribe_video)
+    self.btn_summarize.clicked.connect(self.summarize_video)
+    self.btn_reset.clicked.connect(self.reset_fields)
+
+
+
+if __name__ == '__main__':
+  load_dotenv()
+  api_key_openai = os.getenv("api_key_openai")
+  openai.api_key = api_key_openai
+  
+  app = QApplication(sys.argv)
+
+  myApp = myApp()
+  myApp.show()
+
+  try: 
+    sys.exit(app.exec())
+  except SystemExit:
+    print('Closing Window...')
---
+++ lib/transcriber.py
@@ -0,0 +1,22 @@
+import re
+import openai
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
+
+def get_video_transcript(video_id):
+  pattern = r'\[.*?\]'
+  response = YouTubeTranscriptApi.get_transcript(video_id)
+  transcripts = [item['text'] for item in response]
+  transcripts_string = re.sub(pattern, '', ''.join(transcripts))
+  return transcripts_string
+
+def summarize_transcript(transcript):
+  prompt = 'Please summarize the transcript below into bullet points \n---\n"{0}"'.format
+  response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      temperature=0.1,
+      max_tokens=1000,
+      messages=[
+        {"role": "user", "content": prompt}
+      ]
+    )
+  return response.choices[0]['message']['content']
